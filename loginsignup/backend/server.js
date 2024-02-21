81c2e035d081832cb04b1f7709014e85de95d8ea
shahstuti <114771177+shahstuti@users.noreply.github.com>
2024-02-21 22:07:43 +0530

    Main push

---
+++ loginsignup/backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: "",
+    database: "signup_login"
+})
+
+app.post('/signup', (req, res) => {
+    const sql = "INSERT INTO login (name, email, password) VALUES (?)";
+    const values =[
+        req.body.name,
+        req.body.email,
+        req.body.password
+    ]
+        db.query(sql,[values], (err, data) => {
+            if(err)
+            {
+                return res.json("Error");
+            }
+            return res.json(data);
+        })
+    })
+
+
+    app.listen(3000, () => {
+        console.log('connected');
+    })
